2671db1cd677efbf78f1e0ceb39e4206522f0695
padpc <hostmy@outlook.jp>
2020-02-16 17:46:04 +0900

    form

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {Query} from 'react-apollo'
 import client from './client';
 import {SERCH_REPOSITORIES} from './graphql'
 
-const VARIABLES = {
+const DEFAULT_STATE = {
   first :5,
   after :null,
   last :null,
@@ -16,13 +16,30 @@ const VARIABLES = {
 class  App extends Component {
   constructor(props) {
     super(props)
-    this.state = VARIABLES
+    this.state = DEFAULT_STATE
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({
+      ...DEFAULT_STATE,
+      query:event.target.value
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
   }
 
   render(){
     const { query,first,last,before,after} = this.state
+    console.log({query})
     return (
       <ApolloProvider client={client}>
+        <form>
+          <input value={query} onChange={this.handleChange}/>
+        </form>
         <Query query={SERCH_REPOSITORIES}
                variables={{query,first,last,before,after}} 
         >
