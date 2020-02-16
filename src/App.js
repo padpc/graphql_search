7660be98151603377a544aa35ded502ec066ead9
padpc <hostmy@outlook.jp>
2020-02-16 11:46:28 +0900

    searchrepositories

--- src/App.js
+++ src/App.js
@@ -2,28 +2,43 @@ import React ,{Component} from 'react';
 import {ApolloProvider} from 'react-apollo';
 import {Query} from 'react-apollo'
 import client from './client';
-import {ME} from './graphql.js'
+import {SERCH_REPOSITORIES} from './graphql'
+
+const VARIABLES = {
+  first :5,
+  after :null,
+  last :null,
+  before :null,
+  query :"フロントエンドエンジニア"
+}
 
 
+class  App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = VARIABLES
+  }
 
+  render(){
+    const { query,first,last,before,after} = this.state
+    return (
+      <ApolloProvider client={client}>
+        <Query query={SERCH_REPOSITORIES}
+               variables={{query,first,last,before,after}} 
+        >
+          {
+            ({ loading,error,data}) => {
+              if(loading) return 'Loading...'
+              if(error) return `Error! ${error.message}`
 
-const  App = () => {
-  return (
-    <ApolloProvider client={client}>
-      <div>hello,GraphQl</div>
-
-      <Query query={ME}>
-        {
-          ({ loading,error,data}) => {
-            if(loading) return 'Loading...'
-            if(error) return `Error! ${error.message}`
-
-            return <div>{data.user.name}</div>
+              console.log({data})
+              return <div></div>
+            }
           }
-        }
-      </Query>
-    </ApolloProvider>
-  );
+        </Query>
+      </ApolloProvider>
+    )
+  }
 }
 
 export default App;
--- src/graphql.js
+++ src/graphql.js
@@ -1,8 +1,36 @@
-import gql from 'graphql-tag';
+import gql from 'graphql-tag'
+
+export const SERCH_REPOSITORIES = gql`
+query serchRepositories($first: Int, $after: String, $last: Int, $before: String, $query: String!) {
+  search(first: $first, after: $after, last: $last, before: $before, query: $query, type: REPOSITORY) {
+    repositoryCount
+    pageInfo {
+      endCursor
+      hasNextPage
+      hasPreviousPage
+      startCursor
+    }
+    edges {
+      cursor
+      node {
+        ... on Repository {
+          id
+          name
+          url
+          stargazers {
+            totalCount
+          }
+          viewerHasStarred
+        }
+      }
+    }
+  }
+}
+  `
 
 export const ME = gql`
   query me {
-    user(login:"padpc"){
+    user(login:"iteachonudemy"){
       name
       avatarUrl
     }
