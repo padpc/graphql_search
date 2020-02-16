3209be871f3c98ec958560cbcac16b3164aaceab
padpc <hostmy@outlook.jp>
2020-02-16 10:30:53 +0900

    graphql_clenear

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React ,{Component} from 'react';
 import {ApolloProvider} from 'react-apollo';
-import gql from 'graphql-tag';
 import {Query} from 'react-apollo'
 import client from './client';
+import {ME} from './graphql.js'
 
-const ME = gql`
-  query me {
-    user(login:"padpc"){
-      name
-      avatarUrl
-    }
-  }
-`
+
+
 
 const  App = () => {
   return (
