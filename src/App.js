dec07793064d46bf177308f93757e29e19dfa8c3
padpc <hostmy@outlook.jp>
2020-02-16 10:20:58 +0900

    GraphQL_Client_SetUp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React ,{Component} from 'react';
+import {ApolloProvider} from 'react-apollo';
+import gql from 'graphql-tag';
+import {Query} from 'react-apollo'
+import client from './client';
 
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
-
-console.log({GITHUB_TOKEN});
+const ME = gql`
+  query me {
+    user(login:"padpc"){
+      name
+      avatarUrl
+    }
+  }
+`
 
 const  App = () => {
   return (
-    <div>
-      hello,GraphQl
-    </div>
+    <ApolloProvider client={client}>
+      <div>hello,GraphQl</div>
+
+      <Query query={ME}>
+        {
+          ({ loading,error,data}) => {
+            if(loading) return 'Loading...'
+            if(error) return `Error! ${error.message}`
+
+            return <div>{data.user.name}</div>
+          }
+        }
+      </Query>
+    </ApolloProvider>
   );
 }
 
